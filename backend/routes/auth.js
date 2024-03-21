ab0f420b3587db911de5c9d6f7a28cca11a629e2
rohitthakur111 <rohitthakur.iwdtechnologies@gmail.com>
2024-03-21 18:30:10 +0530

    creating jws token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import User from '../models/Users.js'
 import expressvalidator from 'express-validator';
+import bcrypt from 'bcryptjs'
+import dotenv from 'dotenv'
+import jsonwebtoken from 'jsonwebtoken'
+const jwtToken  = jsonwebtoken;
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
 const { body, validationResult } = expressvalidator;
+const bcript = bcrypt;
 const app = express();
 const router = express.Router();
 
@@ -24,9 +31,21 @@ router.post('/createuser',[
             if(user){
                 return res.status(400).json({ error : 'user is already registered with that account' })
             }
-            user = await User.create(req.body);
-            res.status(201).json({ user });
-           
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+            user = await User.create({
+                name : req.body.name,
+                email : req.body.email, 
+                password : secPass
+            });
+            const data = {
+                user :{
+                    id : user.id
+                }
+            }
+            const authToken = jwtToken.sign(data, JWT_SECRET );
+            // res.status(201).json({ user });  
+            res.status(201).json(authToken);
         }
         catch(err){
             console.log(err);
