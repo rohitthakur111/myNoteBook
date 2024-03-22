46a8af73fbcd22f24cbeaa0cfbe92ff780cf2a99
rohitthakur111 <rohitthakur.iwdtechnologies@gmail.com>
2024-03-22 12:46:18 +0530

    creting a login routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,7 @@ const bcript = bcrypt;
 const app = express();
 const router = express.Router();
 
-// Create a user using Post "/api/auth/createuser" Creating User. || No login required 
+// ROUTE 1: Create a user using Post "/api/auth/createuser" Creating User. || No login required 
 router.post('/createuser',[
     body('name' , 'Enter a valid name').isLength({min : 3}),
     body('email', 'enter a valid name').isEmail(),
@@ -24,15 +24,14 @@ router.post('/createuser',[
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors : errors.array() });
         }
-
         // Check weather user is exists with this email already 
         try{
             let user = await User.findOne({email : req.body.email})
             if(user){
                 return res.status(400).json({ error : 'user is already registered with that account' })
             }
-            const salt = await bcrypt.genSalt(10);
-            const secPass = await bcrypt.hash(req.body.password, salt);
+            const salt = await bcript.genSalt(10);
+            const secPass = await bcript.hash(req.body.password, salt);
             user = await User.create({
                 name : req.body.name,
                 email : req.body.email, 
@@ -54,8 +53,45 @@ router.post('/createuser',[
         
     }catch(error){
         console.log(error);
-        res.status(500).json({ error: error.message });
+        return res.status(500).send("Internal Server Error");
     }
  
 });
+
+// ROUTE 2: Authenticate user  using Post "/api/auth/login" Login User. || No login require
+router.post('/login', [
+    body('email', 'Enter a valid email address').isEmail(),
+    body('password', 'Password cannot be blanked').exists(),
+], async (req,res)=>{
+    try{
+        // if user have a account 
+        const validEmail = validationResult(req);
+        if(!validEmail.isEmpty()){
+            return res.status(200).json({ error : validEmail.array()});
+        }
+        const { email, password } = req.body;
+        const isUser = await User.findOne({ email : req.body.email });
+        if(!isUser){ 
+            console.log('User have no  account on this email address : ' + req.body.email );
+            return res.status(400).json({ error  : 'Login with correct email and password '});
+        }
+        const passwordCompare = await bcript.compare(password , isUser.password);
+        if(!passwordCompare){
+            return res.status(400).json({ error  : 'Login with correct email and password '});
+        }
+        const payLoad = {
+            user : {
+                id : isUser.id
+            }
+        }
+        const loginToken = jwtToken.sign(payLoad, JWT_SECRET);
+        return res.json(loginToken);
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+)
+
+// Exports router 
 export default router;
